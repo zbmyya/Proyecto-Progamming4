9d45c9ad83d99a057e3aaeff4c6151047de4b262
zbmyya <abdel.barona.ajc@gmail.com>
2018-11-29 18:51:55 -0500

    Aqui subo algo.

---
+++ insertar usuario.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.usuarios(
+	nombreu, nombred, apellidou, apellidod, tipdocumento, numdocumento, usuariosis, passwordsis, celular, direccion, email)
+	VALUES ('Zharich', 'Abdel', 'Barona', 'Munioz', 1, 1143872527, 'zbmyya', '1234', '3184285652', 'Avenida 6 oeste', 'zharichb15@gmail.com');
