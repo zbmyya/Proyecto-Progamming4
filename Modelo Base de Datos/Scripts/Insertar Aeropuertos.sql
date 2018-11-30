7922d5f1e08e93952c51c3346d1b27e9d70722ff
zbmyya <abdel.barona.ajc@gmail.com>
2018-11-29 22:53:37 -0500

    Commit de insert de tabla Ciudades y tabla Aeropuerto. Se Modifica la tabla de Aeropuerto para aniadirle el nombre del aeropuerto

--- Modelo Base de Datos/Scripts/Insertar Aeropuertos.sql
+++ Modelo Base de Datos/Scripts/Insertar Aeropuertos.sql
@@ -1,4 +1,71 @@
 INSERT INTO Aeropuerto(
 	codigoIATA,
+	nomAeropuerto,
 	idCiudad)
-	VALUES ('CLO',1);
+	VALUES ('CLO','Aeropuerto Internacional Alfonso Bonilla Aragón',1);
+
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('BOG','Aeropuerto Internacional El Dorado',2);	
+
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('MDE','Aeropuerto Internacional José María Córdova',3);		
+	
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('BAQ','Aeropuerto Internacional Ernesto Cortissoz',4);	
+	
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('PSO','Aeropuerto Antonio Nariño',5);	
+	
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('ADZ','Aeropuerto Internacional Gustavo Rojas Pinilla',6);
+
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('JFK','	Aeropuerto Internacional John F. Kennedy',7);
+
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('BOS','	Aeropuerto Internacional Logan',8);	
+		
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('MIA','	Aeropuerto Internacional de Miami',9);	
+	
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('LAX','Aeropuerto Internacional de Los Ángeles',10);	
+	
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('MAD','Aeropuerto de Madrid-Barajas',11);	
+
+INSERT INTO Aeropuerto(
+	codigoIATA,
+	nomAeropuerto,
+	idCiudad)
+	VALUES ('BCN','Aeropuerto de Barcelona-El Prat',12);		
--- Modelo Base de Datos/Scripts/Insertar Ciudades.sql
+++ Modelo Base de Datos/Scripts/Insertar Ciudades.sql
@@ -17,3 +17,36 @@ INSERT INTO Ciudad(
 INSERT INTO Ciudad(
 	NomCiudad)
 	VALUES ('Pasto');
+
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('San Andres y Providencia');	
+	
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('New York');
+	
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('Boston');	
+
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('Miami');
+	
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('Los Angeles');	
+
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('Madrid');
+	
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('Barcelona');	
+
+INSERT INTO Ciudad(
+	NomCiudad)
+	VALUES ('Paris');
+	
